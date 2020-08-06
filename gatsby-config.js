ac33440e2d2a6752562a02e0a75984e2c18ce001
dsound-zz <demiansims@gmail.com>
2020-08-06 09:23:01 -0400

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
           'gatsby-plugin-react-helmet',
           {
             resolve: 'gatsby-source-contentful',
-            credentials: {
+            options: {
               spaceId: process.env.CONTENTFUL_SPACE_ID,
               accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
             }
