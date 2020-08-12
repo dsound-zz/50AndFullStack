3ca7c2da589d169407e05c42c6cbe445a0c00718
dsound-zz <demiansims@gmail.com>
2020-08-12 08:14:49 -0400

    fixed about

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,9 +15,8 @@ const About = () => {
           coding bootcamp here in NYC. About 4 months after graduating, 
           I was able to land my first tech job as a junior full-stack engineer. I'm proficient in
           HTML5, CSS3, Javascript ES6/7, ReactJS, Redux, Ruby on Rails and
-          familiar with React Native, Node, the AWS platform and of course
-          GatsbyJS which is the framework for this blog! Did I mention I'm passionate about coding? Especially
-          Javascript?
+          familiar with React Native, NodeJS, the AWS platform and of course
+          GatsbyJS which is the framework for this blog! Did I mention I'm passionate about coding? 
         </p>
       </div>
       <p>
