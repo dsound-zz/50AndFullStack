e5dfccff2055c95ba7bfbff1ddc79bc306af5d04
dsound-zz <demiansims@gmail.com>
2020-11-09 21:45:53 -0500

    re-arranged layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,26 +1,18 @@
 import React from "react"
 import Footer from "./footer"
-import Sidebar from "./sidebar"
+import Header from "./header"
 import "../styles/index.scss"
 import layoutStyles from "./layout.module.scss"
-import cx from "classnames"
 
 const Layout = props => {
   return (
     <div className={layoutStyles.container}>
-      <div className={layoutStyles.flexGrid}>
-        <div className={cx(layoutStyles.column, layoutStyles.sidebar)}>
-          <Sidebar />
-        </div>
-        <div className={cx(layoutStyles.column, layoutStyles.content)}>
-          {props.children}
-        </div>
-            <div className={layoutStyles.row}>
-          <div className={layoutStyles.footer}>
-
-            <Footer />
-          </div>
-        </div>
+      <div className={layoutStyles.header}>
+        <Header />
+      </div>
+      <div className={layoutStyles.content}>{props.children}</div>
+      <div className={layoutStyles.footer}>
+        <Footer />
       </div>
     </div>
   )
